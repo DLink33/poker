27d1188795705eefca14a1cd7e977542833cbbb6
David Linkowski <david.linkowski@gmail.com>
2026-01-21 12:37:59 -0700

    Added more robust edible install script

--- pip_edible_install.sh
+++ pip_edible_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
+# check to see if .venv is initialized first
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+# Upgrade pip inside the venv
+./.venv/bin/python -m pip install --upgrade pip
+
 # Directly call the pip inside your venv without "activating"
 ./.venv/bin/python -m pip install -e ".[dev]"
